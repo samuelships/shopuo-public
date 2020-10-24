b8e3dd55c8b827d996f0d7e91c0ac1577a893e8b
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-10-24 02:51:20 +0000

    feat : add firestore service

---
+++ lib/Services/FirestoreService.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<void> setData({String path, Map<String, dynamic> data}) async {
+    await _firestore.doc(path).set(data, SetOptions(merge: true));
+  }
+
+  Future<DocumentSnapshot> getData(String path) async {
+    return await _firestore.doc(path).get();
+  }
+
+  Future<List<T>> getDataCollection<T>({
+    String path,
+    T builder(Map<String, dynamic> data, documentID),
+    Query queryBuilder(Query query),
+  }) async {
+    Query query = _firestore.collection(path);
+
+    if (query != null) {
+      query = queryBuilder(query);
+    }
+
+    return (await query.get())
+        .docs
+        .map((snapshot) => builder(snapshot.data(), snapshot.id))
+        .toList();
+  }
+
+  Future<void> deleteData(String path) async {
+    await _firestore.doc(path).delete();
+  }
+
+  Stream<List<T>> collectionStream<T>({
+    String path,
+    T builder(Map<String, dynamic> data, String documentId),
+    Query queryBuilder(Query query),
+    int sort(T lhs, T rhs),
+  }) {
+    Query query = _firestore.collection(path);
+
+    if (query != null) {
+      query = queryBuilder(query);
+    }
+
+    final snapshots = query.snapshots();
+    return snapshots.map((snapshot) {
+      final result =
+          snapshot.docs.map((item) => builder(item.data(), item.id)).toList();
+
+      if (sort != null) {
+        result.sort(sort);
+      }
+
+      return result;
+    });
+  }
+
+  Stream<T> documentStream<T>(
+      {String path, builder(Map<String, dynamic> data, String documentId)}) {
+    final reference = _firestore.doc(path).snapshots();
+    return reference.map((snap) => builder(snap.data(), snap.id));
+  }
+}
