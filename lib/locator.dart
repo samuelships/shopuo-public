e5cad212ef309a913c9401d9ce2854e1d49d313c
Samuel Besemuna Adjei <51783214+besemuna@users.noreply.github.com>
2020-11-03 09:55:17 +0000

    update locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:shopuo/Screens/SignUpInfo.dart';
 import 'package:shopuo/Services/AuthenticationService.dart';
+import 'package:shopuo/Services/CloudFunctionService.dart';
 import 'package:shopuo/Services/FirestoreService.dart';
 import 'package:shopuo/Services/NavigationService.dart';
 import 'package:shopuo/Services/OverlayService.dart';
@@ -7,9 +9,12 @@ import 'package:shopuo/ViewModels/CartViewModel.dart';
 import 'package:shopuo/ViewModels/CategoriesViewModel.dart';
 import 'package:shopuo/ViewModels/EntryPointViewModel.dart';
 import 'package:shopuo/ViewModels/OnSaleViewModel.dart';
+import 'package:shopuo/ViewModels/ProfileViewModel.dart';
 import 'package:shopuo/ViewModels/ResetPasswordViewModel.dart';
 import 'package:shopuo/ViewModels/SettingsViewModel.dart';
 import 'package:shopuo/ViewModels/SignInViewModel.dart';
+import 'package:shopuo/ViewModels/SignUpInfoViewModel.dart';
+import 'package:shopuo/ViewModels/SignUpVerifyViewModel.dart';
 import 'package:shopuo/ViewModels/SignUpViewModel.dart';
 import './Services/FirebaseMessagingService.dart';
 import 'ViewModels/OverlayManagerViewModel.dart';
@@ -26,10 +31,14 @@ setupLocator() {
   locator.registerFactory(() => CategoriesViewModel());
   locator.registerFactory(() => CartViewModel());
   locator.registerFactory(() => SettingsViewModel());
+  locator.registerFactory(() => ProfileViewModel());
+  locator.registerFactory(() => SignUpInfoViewModel());
+  locator.registerFactory(() => SignUpVerifyViewModel());
 
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => FirestoreService());
   locator.registerLazySingleton(() => AuthenticationService());
   locator.registerLazySingleton(() => OverlayService());
   locator.registerLazySingleton(() => FirebaseMessagingService());
+  locator.registerLazySingleton(() => CloudFunctionService());
 }
